b7ad0a8f1b68885827b335bc7456cda3c3bd6eb7
Matthew Dodd <matthew.dodd93@gmail.com>
2019-02-04 10:10:32 +0100

    Set pug layout and index. Successfully reading and displaying all db books

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const path = require('path');
 const app = express();
 //Database
 const db = require('./config/database');
+app.set('view engine', 'pug');
 
 
 //test db
@@ -12,10 +13,12 @@ db.authenticate()
     .then(() => console.log('connected'))
     .catch(err => console.log('Error: ' + err))
 
-app.get('/', (req, res) => res.send('index'));
+app.get('/', (req, res) => res.redirect('/books'));
 
 //books route
 app.use('/books', require('./routes/books'));
 
-
-app.listen(3000, console.log('server running on port 3000'));
+db.sync()
+.then(() => {
+    app.listen(process.env.PORT || 3000, () => console.log('Application running on localhost:3000'));
+});
