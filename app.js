be43a9809da4efb5b9c74ce5f643fcfad5bfff34
Matthew Dodd <matthew.dodd93@gmail.com>
2019-02-05 17:43:53 +0100

    Edit to error handling

--- app.js
+++ app.js
@@ -22,6 +22,25 @@ app.get('/', (req, res) => res.redirect('/books'));
 //books route
 app.use('/books', require('./routes/books'));
 
+//error handler for 404, using next will pass the err object into the next error handling middleware.
+app.use((req, res, next) => {
+    err = new Error('Not found!');
+    err.status = 404;
+    next(err);
+});
+
+//error handlers always take 4 args, err obect can be passed from a next method.
+app.use((err, req, res, next) => {
+    res.locals.error = err;
+    console.log('Error status:', err.status);
+    res.status(err.status || 500);
+    if(err.status === 404){
+        res.render('errorNotFound');
+    } else {
+        res.render('error');
+    }
+});
+
 db.sync()
 .then(() => {
     app.listen(process.env.PORT || 3000, () => console.log('Application running on localhost:3000'));
